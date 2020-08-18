2fe3fddc1beb3344c68f8d05eb50e698c9b5c328
Darius Lee Pasilaban <p.dariuslee@gmail.com>
2020-08-17 20:47:33 -0700

    server now responds to location request

--- server.js
+++ server.js
@@ -3,31 +3,45 @@
 
 const express = require('express');
 require('dotenv').config(); // reads the file : `.env`
+const cors  = require('cors');
+
 
 
 // ============= Global Variables =============
 
 const PORT = process.env.PORT || 3003; // short cuircuiting and choosing PORT if it exists in the env, otherwise 3003
 const app = express();
-// app.use(cors()); // enables the server to talk to local things
+app.use(cors()); // enables the server to talk to local things
 
 
 
 // ============= Routes =============
 
-// app.get('/location', (request, response) => {
+app.get('/location', (request, response) => {
 
-//   // load json from file
-//   // pass it through the constructor
-//   // send it to the front end
+  // load json from file
+  // pass it through the constructor
+  // send it to the front end
 
-//   const jsonObject = require('./data/location.json');
-//   const constructedLocation = new Location(jsonObject);
+  const jsonObject = require('./data/location.json');
+  const constructedLocation = new Location(jsonObject);
 
-//   response.send(constructedLocation);
+  response.send(constructedLocation);
 
-// });
+});
 
+
+// ============= All other functions =============
+
+
+function Location(jsonObject){
+  console.log(jsonObject);
+
+  this.formatted_query = jsonObject[0].display_name;
+  this.latitude = jsonObject[0].lat;
+  this.longitude = jsonObject[0].lon;
+
+}
 
 
 // ============= Start Server =============
