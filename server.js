706f8bfe0587569d1da203fede948bef2173117b
Darius Lee Pasilaban <p.dariuslee@gmail.com>
2020-08-20 17:43:55 -0700

    movie feature complete

--- server.js
+++ server.js
@@ -35,6 +35,7 @@ const GEOCODE_API_KEY = process.env.GEOCODE_API_KEY;
 const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
 const TRAIL_API_KEY = process.env.TRAIL_API_KEY;
 const DATABASE_URL = process.env.DATABASE_URL;
+const MOVIE_API_KEY = process.env.MOVIE_API_KEY;
 
 // ============= Express configs =============
 
@@ -140,6 +141,30 @@ function sendTrailData(req, res){
     });
 }
 
+
+//route 4
+app.get('/movies', sendMovies);
+
+function sendMovies(req, res){
+
+  let movieTitle = req.query.search_query;
+  const urlToAPIMovies = `https://api.themoviedb.org/3/search/movie?api_key=${MOVIE_API_KEY}&language=en-US&query=${movieTitle}&page=1&include_adult=false`;
+
+
+  superagent.get(urlToAPIMovies)
+    .then(APIMovieData => {
+
+      const movieArr = APIMovieData.body.results.map(movieData => new Movie(movieData));
+      res.send(movieArr);
+
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).send(error.message);
+    });
+}
+
+
 // ============= All other functions =============
 
 
@@ -172,6 +197,16 @@ function Trail(jsonObject){
   this.condition_date = jsonObject.conditionDate.split(' ',1);
   this.condition_time = jsonObject.conditionDate.split(' ')[1];
 
+}
+
+function Movie(jsonObject){
+  this.title = jsonObject.original_title;
+  this.overview = jsonObject.overview;
+  this.average_votes = jsonObject.vote_average;
+  this.total_votes = jsonObject.vote_count;
+  this.image_url = 'https://image.tmdb.org/t/p/w1280' + jsonObject.backdrop_path;
+  this.popularity = jsonObject.popularity;
+  this.released_on = jsonObject.release_date;
 }
 
 // ============= Start Server =============
